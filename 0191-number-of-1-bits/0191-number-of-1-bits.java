5e22e189710a68372353961356c52448c9c143e7
AdityaChauhan05 <officialemail032@gmail.com>
2026-09-03 22:26:13 +0530

    Time: 0 ms (100%), Space: 42.6 MB (11.74%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,17 +1,13 @@
 class Solution {
     public int hammingWeight(int n) {
+        //Brian Kernighan's Algorithm (O(k) time complexity).
 
         int count = 0;
 
-        while(n > 0) { //T.C = O(log n) or 0(32) , S.C = 0(1)
+        while(n > 0) {
 
-        //The n & 1 operation zeroes out everything except the final digit.
-        //If the final digit is 1, the result is 1. If 0, the result is 0.
-
-            if((n & 1) != 0) { //Native bit check
-                count++;
-            }
-            n = n>>1;  //Native bit drop
+            n = n & (n-1);
+            count++;
         }
         return count;
     }
