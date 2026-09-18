49e1a666962a16486bb6408940af507c7bd01f93
AdityaChauhan05 <officialemail032@gmail.com>
2026-09-18 21:43:34 +0530

    Time: 6 ms (100%), Space: 48.4 MB (29.92%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
@@ -1,42 +1,54 @@
-import java.util.*;
-
 class Solution {
-    public List<String> maxNumOfSubstrings(String $) {
-        int __ = $.length();
+    public List<String> maxNumOfSubstrings(String s) {
+        int[] count = new int[26];
+        int[] first = new int[26];
+        int[] last = new int[26];
 
-        int[] ____ = new int[26];  // first occurrence
-        int[] $$ = new int[26];    // last occurrence
-        Arrays.fill(____, -1);
+        Arrays.fill(first, -1);
+        Arrays.fill(last, -1);
 
-        for (int $_ = 0; $_ < __; $_++) {
-            int $$_ = $.charAt($_) - 'a';
-            if (____[$$_] == -1) ____[$$_] = $_;
-            $$[$$_] = $_;
-        }
+        List<Integer> order = new ArrayList<>();
 
-        List<String> ________ = new ArrayList<>();
-        int _________ = -1;  // right edge of the last interval we kept
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i) - 'a';
 
-        for (int $_ = 0; $_ < __; $_++) {
-            if (____[$.charAt($_) - 'a'] != $_) continue;  // only first occurrences can start
-
-            int _____ = $$[$.charAt($_) - 'a'];  // expanding right edge
-            boolean ______ = true;               // still self-contained?
-
-            for (int $__ = $_; $__ <= _____; $__++) {
-                int _______ = $.charAt($__) - 'a';
-                if (____[_______] < $_) { ______ = false; break; }
-                if ($$[_______] > _____) _____ = $$[_______];
+            if (count[c] == 0) {
+                first[c] = i;
+                order.add(c);
             }
 
-            if (!______) continue;
-
-            if ($_ > _________) ________.add($.substring($_, _____ + 1));
-            else ________.set(________.size() - 1, $.substring($_, _____ + 1));
-
-            _________ = _____;
+            count[c]++;
+            last[c] = i;
         }
 
-        return ________;
+        List<String> res = new ArrayList<>();
+        Deque<int[]> queue = new ArrayDeque<>();
+
+        for (int c : order) {
+            queue.addFirst(new int[]{
+                first[c], last[c], count[c]
+            });
+
+            int left = Integer.MAX_VALUE;
+            int right = Integer.MIN_VALUE;
+            int total = 0;
+
+            for (int[] item : queue) {
+                total += item[2];
+                left = Math.min(left, item[0]);
+                right = Math.max(right, item[1]);
+
+                if (total == right - left + 1) {
+                    break;
+                }
+            }
+
+            if (total == right - left + 1) {
+                res.add(s.substring(left, right + 1));
+                queue.clear();
+            }
+        }
+
+        return res;
     }
 }
